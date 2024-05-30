a2c1a5a0422d136b5e4fd5e032330fcd55502378
Keinzeit <166651700+Keinzeit@users.noreply.github.com>
2024-05-30 11:15:28 -0600

    Update RPG.py
    Adds the bar drawing function

--- RPG.py
+++ RPG.py
@@ -2,6 +2,7 @@
 
 import os
 import time
+import math
 
 #Function to clear console
 def clear(t = 2):
@@ -13,22 +14,40 @@ def clear(t = 2):
         os.system("clear");
 
 def startStats():
+    #TODO make an object
     HP = 5;
     df = 2;
     atk = 1;
     exp = 0;
 
+#Fills the HP bar with '=' acording to the current HP, it has 10 parts
+
+def drawBar(name, totalHP, currentHP):
+    bar = "";
+    targetDraw = math.ceil(currentHP * 10 / totalHP);
+
+    for i in range(10):
+        if i < targetDraw:
+            bar += "="
+        else:
+            bar += " "
+
+    print(name + " HP\t[" + bar + "]");
+
+#TODO attack function
+#TODO enemy list
+
 #main loop
 
 finishGame = False;
-
 playerName = input("Welcome to kzn's RPG\nWrite your name: ");
 startStats();
 
 while not finishGame:
+    drawBar(playerName, 10, 10);
     playerAction = input("Choose your action:\tAttack\t Finish\n");
     playerAction = playerAction.strip().lower();
-    
+
     if playerAction == "finish":
         finishGame = True;
         print("You chose to", playerAction);
